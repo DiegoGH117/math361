dd1af69cb85a6c1ac92c6d9fcc976b7a451942dc
Diego Gonzalez <djgh.117@gmail.com>
2020-08-23 20:26:42 -0400

    script for first problem in hw1 completed

---
+++ HW1/PlotCircleHexagon.m
@@ -0,0 +1,30 @@
+%
+%   Script: PlotCircleHexagon
+%
+%   This script plots a blue circle of radius 1 with a red
+%   regular hexagon inside. This corresponds to Problem 1 of
+%   Homework 1
+
+% Create the angles for the circle and hexagon using r=1 as in the figure
+thetac = linspace(0, 2*pi, 201); 
+thetah = linspace(0, 2*pi, 7);
+
+% Calculate the (x,y) pairs for both shapes
+r = 1;
+xc = r*cos(thetac);
+yc = r*sin(thetac);
+
+xh = r*cos(thetah);
+yh = r*sin(thetah);
+
+% Create the plot
+LW = 2;
+plot(xc,yc, 'Color', 'r', 'LineWidth', LW), hold on % circle is red
+plot(xh,yh, 'b-o', 'MarkerFaceColor', 'b', 'LineWidth', LW) % hexagon is blue with filled dots
+
+% change axes' limits
+axis([-1.25*r,1.25*r,-1*r,1*r])
+
+
+
+
