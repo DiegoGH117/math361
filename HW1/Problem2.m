3c394f8b7544e9f016ce9878ef82603a48eed1bd
Diego Gonzalez <djgh.117@gmail.com>
2020-08-24 13:11:11 -0400

    finished the second problem of the first homework

--- HW1/Problem2.m
+++ HW1/Problem2.m
@@ -18,4 +18,27 @@ mesh(X1,Y1,F)
 figure(2)
 contour(X1,Y1,F)
 
+% Create the second meshgrid
+[X2,Y2] = meshgrid(-2:.1:2,-4:.1:4);
+
+% Calculate the result of the second function
+G = (-3*Y2./(X2.^2+Y2.^2+1));
+
+% create plots, first mesh and then contour, in different figures
+figure(3)
+mesh(X2,Y2,G)
+figure(4)
+contour(X2,Y2,G)
+
+% Create the second meshgrid
+[X3,Y3] = meshgrid(-2:.1:2,-1:.1:1);
+
+% Calculate the result of the second function
+H = abs(X3) + abs(Y3);
+
+% create plots, first mesh and then contour, in different figures
+figure(5)
+mesh(X3,Y3,H)
+figure(6)
+contour(X3,Y3,H)
 
