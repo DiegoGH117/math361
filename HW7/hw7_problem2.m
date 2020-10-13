248b859c2b3e9af23789a64a00b1f908f6fe95ea
Diego Gonzalez <djgh.117@gmail.com>
2020-10-13 05:58:45 -0400

    almost done with a)

--- HW7/hw7_problem2.m
+++ HW7/hw7_problem2.m
@@ -21,15 +21,30 @@ x = 1;
 n = 0:16;
 h = 10.^(-n);
 
+% calculate the approximate derivatives using the first order
+% approximation 
+df1 = (f(x+h) - f(x))./(h);
+
+% calculate the approximate derivatives using the second order
+% approximation 
+df2 = (f(x+h) - f(x-h))./(2*h);
+
 % calculate the approximate derivatives using the second order one-sided
 % approximation given in problem 1
-df = (-3*f(x) + 4*f(x+h) - f(x+2*h))./(2*h);
+df3 = (-3*f(x) + 4*f(x+h) - f(x+2*h))./(2*h);
 
 % print out the results in a nicely formatted table
-fprintf(1,'===================================\n');
-fprintf(1,'    n      Numerical Derivative   \n');
-fprintf(1,'-----------------------------------\n');
+fprintf(1,'NUMERICAL DERIVATIVES:===========================================================\n');
+fprintf(1,'    n           First Order            Second Order         2nd. Order 1 Sided   \n');
+fprintf(1,'---------------------------------------------------------------------------------\n');
 for j = 1:length(h)
-    fprintf(1,'   %2d       %17.14f\n', n(j), df(j));
+    fprintf(1,'   %2d       %17.14f       %17.14f       %17.14f\n', n(j), df1(j), df2(j), df3(j));
 end
-fprintf(1,'===================================\n');
+fprintf(1,'=================================================================================\n');
+
+
+% now we need to compute the absolute value of the error that each one of
+% these approximations has, to do so, we analitically compute the
+% derivative of f(x) and use a function handle 
+
+fprime = 1; %PUT HERE THE ANALYTICAL DERIVATIVE
